3e38f57b1c6eefed9ea6c8a35f7607e961ad5da8
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2022-03-08 13:37:04 -0500

    create employee and person classes

---
+++ employee.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Author: Logan Orender
+ * Date: 3/8/2022
+ * File: employee.class.php
+ * Description:
+ */
+class Employee implements Payable
+{
+
+    public function getPaymentAmount()
+    {
+        // TODO: Implement getPaymentAmount() method.
+    }
+
+    public function toString()
+    {
+        // TODO: Implement toString() method.
+    }
+}
---
+++ person.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Author: Logan Orender
+ * Date: 3/8/2022
+ * File: person.class.php
+ * Description:
+ */
+class Person
+{
+
+}
