22b9af478fd8863c05b385911474ff75e72531dc
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2022-03-08 13:50:20 -0500

    Complete person.class.php
    minus comments of course, cuz those are for posers.

--- person.class.php
+++ person.class.php
@@ -8,5 +8,33 @@
  */
 class Person
 {
+    private $first_name;
+    private $last_name;
 
+    public function __construct($first_name,$last_name)
+    {
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    public function toString()
+    {
+        return "$this->first_name" . "$this->last_name";
+    }
 }
