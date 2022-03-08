fe0339b084fbb802e058ac70f13119a5950d2bac
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2022-03-08 14:13:12 -0500

    update person and complete employee

--- employee.class.php
+++ employee.class.php
@@ -6,16 +6,44 @@
  * File: employee.class.php
  * Description:
  */
-class Employee implements Payable
+abstract class Employee implements Payable
 {
+    private $person;
+    private $ssn;
+    private static $employee_count;
 
-    public function getPaymentAmount()
+    private function __construct($person, $ssn)
     {
-        // TODO: Implement getPaymentAmount() method.
+        $this->person = $person;
+        $this->ssn = $ssn;
+        self::$employee_count++;
     }
 
-    public function toString()
+    public function getPerson()
     {
-        // TODO: Implement toString() method.
+        return $this->person;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getSSN()
+    {
+        return $this->ssn;
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function getEmployeeCount()
+    {
+        return self::$employee_count;
+    }
+
+    abstract public function getPaymentAmount();
+
+    public function toString() {
+        return $this->person->toString() . "SSN: $this->ssn ";
+    }
+
 }
